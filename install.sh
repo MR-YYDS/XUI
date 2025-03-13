4347b0db4f303defd35336568c51acc23960f02d
YYDS <125422525+MR-YYDS@users.noreply.github.com>
2025-03-13 13:55:20 +0800

    Personalize install.sh

--- install.sh
+++ install.sh
@@ -33,8 +33,6 @@ arch=$(arch)
 
 if [[ $arch == "x86_64" || $arch == "x64" || $arch == "s390x" || $arch == "amd64" ]]; then
     arch="amd64"
-elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-    arch="arm64"
 else
     arch="amd64"
     echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
